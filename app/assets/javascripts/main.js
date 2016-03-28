2feaf371d67c0c8416ed112f47959bc8ba94058d
Sylvain Martin <sylvainmdev@gmail.com>
2016-03-28 20:16:40 +0200

    Add favicon, change fab button to a link_to button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,11 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
+   
+   $('.dropdown-button').dropdown({
+     inDuration: 300,
+     outDuration: 225,
+     gutter: 0, // Spacing from edge
+     belowOrigin: true, // Displays dropdown below the button
+   }
+ );
 })
